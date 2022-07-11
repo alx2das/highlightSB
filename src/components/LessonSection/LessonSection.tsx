f9fa3589ba8612c81c6958326ff8524263ebf0cd
Alexandr Buylov <a.buylov@pnlfin.tech>
2022-07-11 12:28:37 +0300

    add LessaonSection

---
+++ src/components/LessonSection/LessonSection.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from "react";
+
+
+const LessonSection: FC = () => {
+    return (
+        <div>
+            <h1>Title</h1>
+            <h3>Subtitle</h3>
+            <div>
+                description
+            </div>
+            <div>
+                IMAGE
+            </div>
+        </div>
+    );
+};
+
+
+export default LessonSection;
